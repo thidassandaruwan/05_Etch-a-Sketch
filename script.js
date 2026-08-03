4113d9a6c9ca6d61d89bd5735aaf4a834dcf7f30
thidassandaruwan <170103578+thidassandaruwan@users.noreply.github.com>
2026-08-03 13:54:36 +0530

    Implement pixel color changing on hover

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
+// triggering initial 16 x 16
+document.addEventListener("DOMContentLoaded", () => renderDrawingBoard(16))
+
+// rendering elements
 const drawingBoard = document.querySelector(".drawing-board");
 const boardSize = document.querySelector("#drawing-board-size");
 
-// class="pixel"
-function renderPixels(number){
+// input elements
+// select buttons
+const pressHoldBtn = document.querySelector("#press-hold-btn");
+const eraseBtn = document.querySelector("#erase-btn");
+const autoColorBtn = document.querySelector("#auto-color-btn");
+// regular buttons
+const clearBtn = document.querySelector("#clear-btn");
+const resizeBtn = document.querySelector("#resize-btn");
+const randomColorBtn = document.querySelector("#random-color-btn");
+// color selector
+const colorSample = document.querySelector("#color-sample");
+
+// event listners
+// resize
+resizeBtn.addEventListener("click", () => {
+    let boardSize;
+    do {
+        boardSize = parseInt(prompt("Enter drawing board size you want : "));
+        boardSize = ((10 > boardSize) || (boardSize > 100)) ? NaN : boardSize;
+        
+    } while (Number.isNaN(boardSize));
+
+    renderDrawingBoard(boardSize);
+})
+
+// draw on hover
+drawingBoard.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("pixel")){
+        const targetPixel = event.target;
+        const pixelColor = colorSample.value;
+
+        targetPixel.style.backgroundColor = pixelColor;
+    }
+});
+
+
+
+// functions
+function renderDrawingBoard(number){
     drawingBoard.replaceChildren();
 
     const boardFragment = document.createDocumentFragment();
@@ -17,7 +58,5 @@ function renderPixels(number){
     drawingBoard.appendChild(boardFragment);
     document.documentElement.style.setProperty("--grid-size", number);
     boardSize.textContent = `${number} x ${number}`;
-
 }
 
-renderPixels(100);
